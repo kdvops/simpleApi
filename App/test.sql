05e2837d2f953a39da1507c975cc49b21faab8bd
kdvops <kelvin.ag89@gmail.com>
2025-11-16 02:38:13 -0400

    Add files via upload

---
+++ App/test.sql
@@ -0,0 +1,9 @@
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    email VARCHAR(100)
+);
+
+INSERT INTO usuarios (nombre,email) VALUES
+('Kelvin Alcala','kelvin@kdvops.com'),
+('Juan Perez','jperez@mail.com');

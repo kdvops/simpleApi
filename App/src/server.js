5326cdbf14eabd722d2a341cb8e16004e3b0e150
kdvops <kelvin.ag89@gmail.com>
2025-11-16 10:45:10 -0400

    Update server.js

--- App/src/server.js
+++ App/src/server.js
@@ -8,7 +8,10 @@ app.use(express.json());
 
 app.use('/api', router);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`API running on port ${PORT}`);
-});
+export default app; // ⬅ necesario para testing
+
+// Solo iniciar servidor si no estamos en test
+if (process.env.NODE_ENV !== "test") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`API running on port ${PORT}`));
+}

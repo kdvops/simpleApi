5a0716b238c5cb3df567d8c31e1da3b03ae60e34
kdvops <kelvin.ag89@gmail.com>
2025-11-23 20:30:00 -0400

    Modify DB connection settings in db.js
    Updated database connection configuration to use hardcoded host and user values while keeping password and database name from environment variables.

--- App/src/db.js
+++ App/src/db.js
@@ -3,8 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    //host: process.env.DB_HOST,
+    //user: process.env.DB_USER,
+    //password: process.env.DB_PASSWORD,
+    //database: process.env.DB_NAME,
+    host: "database-1.c29y2kmw0cj4.us-east-1.rds.amazonaws.com",
+    user: "root",
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
@@ -12,3 +16,4 @@ const pool = mysql.createPool({
 });
 
 export default pool;
+
